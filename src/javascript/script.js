6b22f453e66ab7fae167a015eb3b52ef5f095435
Igor Lima Miranda <ig.lm123@gmail.com>
2025-10-07 19:52:03 -0400

    Fazendo o JS do Projeto Kanban

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,1 +1,16 @@
-﻿
+﻿document.querySelectorAll('.kanban-card').forEach(card => {
+    card.addEventListener('dragstart', e => {
+        e.currentTarget.classList.add('dragging');
+    })
+
+    card.addEventListener('dragend', e => {
+        e.currentTarget.classList.remove('dragging');
+    })
+})
+
+document.querySelectorAll('.kanban-cards').forEach(column => {
+    column.addEventListener('dragover', e => {
+        e.preventDefault();
+        e.currentTarget.classList.add('cards-hover');
+    })
+})
