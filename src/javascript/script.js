4e3a1b16f48635f9e4558198d54893a741db0157
Igor Lima Miranda <ig.lm123@gmail.com>
2025-10-12 11:47:15 -0400

    Exemplo do Código JS Puro em Jquery

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,28 +1,59 @@
-﻿document.querySelectorAll('.kanban-card').forEach(card => {
-    card.addEventListener('dragstart', e => {
-        e.currentTarget.classList.add('dragging');
-    })
+﻿// #region JavaScript Puro
+// document.querySelectorAll('.kanban-card').forEach(card => {
+//     card.addEventListener('dragstart', e => {
+//         e.currentTarget.classList.add('dragging');
+//     })
 
-    card.addEventListener('dragend', e => {
-        e.currentTarget.classList.remove('dragging');
-    })
-})
+//     card.addEventListener('dragend', e => {
+//         e.currentTarget.classList.remove('dragging');
+//     })
+// })
 
-document.querySelectorAll('.kanban-cards').forEach(column => {
-    column.addEventListener('dragover', e => {
+// document.querySelectorAll('.kanban-cards').forEach(column => {
+//     column.addEventListener('dragover', e => {
+//         e.preventDefault();
+//         e.currentTarget.classList.add('cards-hover');
+//     })
+
+//     column.addEventListener('dragleave', e => {
+//         e.currentTarget.classList.remove('cards-hover');
+//     })
+
+//     column.addEventListener('drop', e => {
+//         e.currentTarget.classList.remove('cards-hover');
+
+//         const dragCard = document.querySelector('.kanban-card.dragging');
+
+//         e.currentTarget.appendChild(dragCard);
+//     })
+// })
+// #endregion
+
+// #region jQuery
+$(function () {
+    $('.kanban-card').on('dragstart', function () {
+        $(this).addClass('dragging');
+    });
+
+    $('.kanban-card').on('dragend', function () {
+        $(this).removeClass('dragging');
+    });
+
+    $('.kanban-cards').on('dragover', function (e) {
         e.preventDefault();
-        e.currentTarget.classList.add('cards-hover');
-    })
+        $(this).addClass('cards-hover');
+    });
 
-    column.addEventListener('dragleave', e => {
-        e.currentTarget.classList.remove('cards-hover');
-    })
+    $('.kanban-cards').on('dragleave', function () {
+        $(this).removeClass('cards-hover');
+    });
 
-    column.addEventListener('drop', e => {
-        e.currentTarget.classList.remove('cards-hover');
+    $('.kanban-cards').on('drop', function (e) {
+        e.preventDefault();
+        $(this).removeClass('cards-hover');
 
-        const dragCard = document.querySelector('.kanban-card.dragging');
-
-        e.currentTarget.appendChild(dragCard);
-    })
-})
+        const $dragCard = $('.kanban-card.dragging');
+        $(this).append($dragCard);
+    });
+});
+// #endregion
