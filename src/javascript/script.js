76910d4a679374a7fb7019e9fba81be3d4dfa3a0
Igor Lima Miranda <ig.lm123@gmail.com>
2025-10-12 11:14:29 -0400

    Finalizado Tutorial KANBAN arrasta e solta usando HTML, CSS e JavaScript

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -13,4 +13,16 @@ document.querySelectorAll('.kanban-cards').forEach(column => {
         e.preventDefault();
         e.currentTarget.classList.add('cards-hover');
     })
+
+    column.addEventListener('dragleave', e => {
+        e.currentTarget.classList.remove('cards-hover');
+    })
+
+    column.addEventListener('drop', e => {
+        e.currentTarget.classList.remove('cards-hover');
+
+        const dragCard = document.querySelector('.kanban-card.dragging');
+
+        e.currentTarget.appendChild(dragCard);
+    })
 })
